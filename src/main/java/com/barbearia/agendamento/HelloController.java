aca7c4271d7fa8a47c0b523c460e5f8bb94683d8
João Gabriel <jogabriel37@gmail.com>
2026-06-03 00:23:53 -0300

    feat: estrutura inicial do projeto de agendamento

---
+++ src/main/java/com/barbearia/agendamento/HelloController.java
@@ -0,0 +1,13 @@
+package com.barbearia.agendamento;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Barbearia API funcionando!";
+    }
+}
